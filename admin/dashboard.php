7e57c561f48ce7ba5b2a4335af2effa61ad87506
Aini139 <ainq139@gmail.com>
2024-11-23 22:59:23 +0500

    upload

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -71,6 +71,8 @@
             <!-- fix for small devices only -->
             <div class="clearfix hidden-md-up"></div>
 
+
+            <!-- like mainy ye change ki hai aur push karny laga ho ok  -->
             <div class="col-12 col-sm-6 col-md-3">
                 <div class="info-box mb-3">
                     <span class="info-box-icon bg-success elevation-1"><i class="fas fa-book-open"></i></span>
